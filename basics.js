a5e1b4d82a34e051b2b0c99f5e0491b00f820b78
andepushpakumar <andepushpakumar@gmail.com>
2026-01-23 18:18:15 +0530

    my files

---
+++ basics.js
@@ -0,0 +1,20 @@
+
+console.log("Hello, World!");
+console.log("This is a basic JavaScript file.");    
+var a=10
+let b=3;
+
+console.log( a,b);
+
+var string="This is a string";
+console.log(string);
+var boolean=true;
+console.log(boolean);   
+var n=null;
+console.log(n); 
+var u=undefined;
+console.log(u);
+var Number=12345;
+console.log(Number);
+var bigInt=9007199254740991n;
+console.log(bigInt); 
